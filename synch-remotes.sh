6d2b3aa8852c4040c27aa48f647a8db5714263e7
Cornelio Hopmann <cornelio.hopmann@wobcom.de>
2020-04-20 12:11:14 +0200

    ADD: pgadmin4 chart

--- synch-remotes.sh
+++ synch-remotes.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+cd $(dirname `[[ $0 = /* ]] && echo "$0" || echo "$PWD/${0#./}"`)
+
 mkdir tmp
 cd tmp
-git clone https://github.com/gravitee-io/gravitee-kubernetes.git && rm -rf gravitee-kubernetes/.git
-rm gravitee-kubernetes/gravitee/requirements.lock
-rm gravitee-kubernetes/gravitee/requirements.yaml
-cp -R gravitee-kubernetes/gravitee ../charts
-git clone https://github.com/stakater-charts/pgadmin.git && rm -rf pgadmin/.git
-cp -R pgadmin/pgadmin ../charts
+# git clone https://github.com/gravitee-io/gravitee-kubernetes.git && rm -rf gravitee-kubernetes/.git
+# rm gravitee-kubernetes/gravitee/requirements.lock
+# rm gravitee-kubernetes/gravitee/requirements.yaml
+# cp -R gravitee-kubernetes/gravitee ../charts
+git clone https://github.com/rowanruseler/helm-charts.git && rm -rf helm-charts/.git
+cp -R helm-charts/charts/pgadmin4 ../charts
 cd ..
 rm -rf tmp
